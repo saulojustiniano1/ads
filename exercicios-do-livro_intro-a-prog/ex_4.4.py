ae214aa5775c1efa7f473e6e131de75ec550c53e
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-03-12 12:02:17 -0300

    📦create: exercicios-livro - if | else | elif

---
+++ exercicios-do-livro_intro-a-prog/ex_4.4.py
@@ -0,0 +1,3 @@
+# Exercício 4.4 - Escreva um programa que pergunte o salário do funcionário e calcule
+# o valor do aumento. Para salários superiores a R$ 1.250,00, calcule um aumento de
+# 10%. Para os inferiores ou iguais, de 15%.
